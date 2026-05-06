bb9b1232010fdaeafccfe71aae117c9daa402baf
BastiMan-X5 <darkblazingod@gmail.com>
2026-05-06 02:08:04 +0000

    Desactivando barra de progreso para evitar error de hilos

--- sample-app.sh
+++ sample-app.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# 1. Limpieza de carpetas temporales
 rm -rf tempdir
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
 
-# 2. Copia de archivos locales a la carpeta temporal
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-# 3. Creación del Dockerfile (Usando la versión slim y optimizada)
 echo "FROM python:3.9-slim" > tempdir/Dockerfile
-echo "RUN pip install --no-cache-dir flask" >> tempdir/Dockerfile
+echo "RUN pip install --no-cache-dir --progress-bar off flask" >> tempdir/Dockerfile
 echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
-# 4. Construcción y despliegue
 cd tempdir
 docker build -t sampleapp .
 docker stop samplerunning || true
